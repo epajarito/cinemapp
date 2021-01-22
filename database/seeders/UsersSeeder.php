fcf986f3800040031664d055d8cb8f8979980cd4
Eduardo Pajarito <laalocuautle@gmail.com>
2021-01-21 20:38:17 -0600

    login

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+        ]);
+        User::factory()->times(1)->create();
+    }
+}
